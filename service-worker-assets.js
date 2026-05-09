b6a3e2ce6ed4643f00910d79d4936c27289fbb42
Hermilo Santiago Benito <hermilocap@hotmail.com>
2026-05-09 10:54:34 -0600

    Se agrega deployment de la primera version de maqsaf

---
+++ service-worker-assets.js
@@ -0,0 +1,225 @@
+self.assetsManifest = {
+  "version": "c+2vnvLH",
+  "assets": [
+    {
+      "hash": "sha256-ifgQZgaEpvnmB8MSKL3I7sSU4cQsT8agwvJcvNzHe10=",
+      "url": "404.html"
+    },
+    {
+      "hash": "sha256-z2v6MQ6Fiy96r4hERF0SLq4skkuCeZGlDMbQwosxT+A=",
+      "url": "_framework/Microsoft.AspNetCore.Components.Forms.wasm"
+    },
+    {
+      "hash": "sha256-LrDNrW/bydqUtuFUrSugUIXzJcocI+apjmLKg8L0u1w=",
+      "url": "_framework/Microsoft.AspNetCore.Components.Web.wasm"
+    },
+    {
+      "hash": "sha256-a7yMQli0i9AP4PByI97RlJtKSzeFvFzgMGalH98PVxM=",
+      "url": "_framework/Microsoft.AspNetCore.Components.WebAssembly.wasm"
+    },
+    {
+      "hash": "sha256-oRu18HDtAeS5M4lvksadxiyqh4FMwOaeQeGY2cV9+QQ=",
+      "url": "_framework/Microsoft.AspNetCore.Components.wasm"
+    },
+    {
+      "hash": "sha256-XR+WUfg8LtnrwdXFBnV+qO3ROyp6cYCfl1AZajIAheM=",
+      "url": "_framework/Microsoft.Extensions.Configuration.Abstractions.wasm"
+    },
+    {
+      "hash": "sha256-3UBRwvCkKtraRUarsIBDS05tA2rRm1leCB6EDfyVjeA=",
+      "url": "_framework/Microsoft.Extensions.Configuration.Json.wasm"
+    },
+    {
+      "hash": "sha256-od574rRtVVRJj+24jbl8j3GdQFpxlJOpSx+pEMw1Q18=",
+      "url": "_framework/Microsoft.Extensions.Configuration.wasm"
+    },
+    {
+      "hash": "sha256-ChivLe7SrJDCDJ1peRj6DU1QYnPM/+MnXcM7Staf1fI=",
+      "url": "_framework/Microsoft.Extensions.DependencyInjection.Abstractions.wasm"
+    },
+    {
+      "hash": "sha256-mw/Sy1n9J6R0bUD0GI5MtB/zP+wyzOPiVsOTA/1C4NA=",
+      "url": "_framework/Microsoft.Extensions.DependencyInjection.wasm"
+    },
+    {
+      "hash": "sha256-M9haEJgkfushCmQftqPfw27aLKCdYcUnsaC38TcfEpk=",
+      "url": "_framework/Microsoft.Extensions.Logging.Abstractions.wasm"
+    },
+    {
+      "hash": "sha256-lronxO5RVywLbFhme/ExIuzFePlP8EosZyZteUaMAwE=",
+      "url": "_framework/Microsoft.Extensions.Logging.wasm"
+    },
+    {
+      "hash": "sha256-jU67Fez8pypOqHShO/C6RHAZmEFgoq4RZGZtcWXOF08=",
+      "url": "_framework/Microsoft.Extensions.Options.wasm"
+    },
+    {
+      "hash": "sha256-xyqqv0SMCahnrOs+p5QYDHoh/BAYlbNtyjTTpqmRW/g=",
+      "url": "_framework/Microsoft.Extensions.Primitives.wasm"
+    },
+    {
+      "hash": "sha256-Px6z5DU/gBGylV4ob9sdcAVH4KKxpByP7WNTn+lZUro=",
+      "url": "_framework/Microsoft.JSInterop.WebAssembly.wasm"
+    },
+    {
+      "hash": "sha256-5YCTBeqPLBCgnNIAwn6sgKyBKS13Ooms1CiP1kN6el8=",
+      "url": "_framework/Microsoft.JSInterop.wasm"
+    },
+    {
+      "hash": "sha256-Ld0E4JwGKw2ue/cpVkKoh+V6GvIcEkoDNwLg3+Cz6oI=",
+      "url": "_framework/System.Collections.Concurrent.wasm"
+    },
+    {
+      "hash": "sha256-w1R1YSvN/9YPscSEP3CGMKvCDjcVazYZFaJgSGZ3QDU=",
+      "url": "_framework/System.Collections.NonGeneric.wasm"
+    },
+    {
+      "hash": "sha256-TMn14zbESbQlfSn63/NJGGxBbD9+D2uUlK9Cy+FHBWQ=",
+      "url": "_framework/System.Collections.Specialized.wasm"
+    },
+    {
+      "hash": "sha256-nA2Yd8rgsCQRSuQY9SIZ7wUciw6dA0S/UTc/88ffQX4=",
+      "url": "_framework/System.Collections.wasm"
+    },
+    {
+      "hash": "sha256-5RrGfG4yIM/eardH0tg49mYAki1z8z7Ks4sVM72BZwU=",
+      "url": "_framework/System.ComponentModel.Annotations.wasm"
+    },
+    {
+      "hash": "sha256-oyB/W8nbv4QvjI1mX9t5OWXpTSoXz9IKJHbtQqMAGSU=",
+      "url": "_framework/System.ComponentModel.Primitives.wasm"
+    },
+    {
+      "hash": "sha256-yPWWrdnFM6jbIaBOKlryF4BhnLobZWWr8lzSLBl028g=",
+      "url": "_framework/System.ComponentModel.TypeConverter.wasm"
+    },
+    {
+      "hash": "sha256-QKYEp53FBtgYISDWRpVq6fZWEEQDfv5Uq1vzmKnPjvY=",
+      "url": "_framework/System.ComponentModel.wasm"
+    },
+    {
+      "hash": "sha256-ltUpHJAR+rSY7E6Ygf1YJjzECKJQEb9CRLVlpDDiB1o=",
+      "url": "_framework/System.Console.wasm"
+    },
+    {
+      "hash": "sha256-HGc5RuBhlMhFUxiMq3ZsAS7F5LNRSwmdHCOhQ86GVTU=",
+      "url": "_framework/System.Linq.Expressions.wasm"
+    },
+    {
+      "hash": "sha256-1KXEKyggiVVS6WV4z0O4HWHLMl9STF9QCGXD1jPsxT4=",
+      "url": "_framework/System.Linq.wasm"
+    },
+    {
+      "hash": "sha256-IBBpY14PKWED/JsSpBkZ62dxs263fWVCQp7OlcTWVhk=",
+      "url": "_framework/System.Memory.wasm"
+    },
+    {
+      "hash": "sha256-WAWtmZPy2gpdr1wiOHAj2KLfOEopIW07RaEYXQpgkKY=",
+      "url": "_framework/System.Net.Http.wasm"
+    },
+    {
+      "hash": "sha256-MlSfEMhYoxqfu51UpKtzk7Q8MCRCyhvMvNc17Nsmois=",
+      "url": "_framework/System.Net.Primitives.wasm"
+    },
+    {
+      "hash": "sha256-YLKuW8F9StHTvmkRUVyqBiMG4o3kOK/dT0d4iXBjq2w=",
+      "url": "_framework/System.ObjectModel.wasm"
+    },
+    {
+      "hash": "sha256-aqtGoO/SGLz9twExOlYJ8xabryk9GnHS9HGTCuDxHsY=",
+      "url": "_framework/System.Private.CoreLib.wasm"
+    },
+    {
+      "hash": "sha256-5O81Tag3mcXI5igGcNFV29KAZMkq5gbn8LDkwhhZMHs=",
+      "url": "_framework/System.Private.Uri.wasm"
+    },
+    {
+      "hash": "sha256-d7v+LFbiOLx/xxM/haKFVSGNCCkxJoSZqBNIv/vOpYo=",
+      "url": "_framework/System.Runtime.InteropServices.JavaScript.wasm"
+    },
+    {
+      "hash": "sha256-Pf0YH2p89x+CN4oRCUdVLSTHSxKcwlQ4yymem+Ez3vU=",
+      "url": "_framework/System.Runtime.Serialization.Primitives.wasm"
+    },
+    {
+      "hash": "sha256-rSLIJmIBZBtvU/0bepM5Lv/qsxr/gBu3Z9A+LnyhwkQ=",
+      "url": "_framework/System.Runtime.wasm"
+    },
+    {
+      "hash": "sha256-qBa/Lg3ErwYkMwsGtKg9uWei1d20QlVrJNMTDVB74bw=",
+      "url": "_framework/System.Text.Encodings.Web.wasm"
+    },
+    {
+      "hash": "sha256-qoclxa2KxYQl8mSwfmuqoX5A9tOxLeBEq/E08QoI0yA=",
+      "url": "_framework/System.Text.Json.wasm"
+    },
+    {
+      "hash": "sha256-RH+1I9pL6sbT/sJ536fyV8GYZ70k3wI/XNaAf+KtRQg=",
+      "url": "_framework/System.Text.RegularExpressions.wasm"
+    },
+    {
+      "hash": "sha256-U1ugCDIgbEW6lR5UkMCUZh1lhLDB62fhi0ZthK9FNyg=",
+      "url": "_framework/System.wasm"
+    },
+    {
+      "hash": "sha256-1w88C6QZFe/3qmUMMFZHWtFj/IeaDWO9gDhgKlEhV0c=",
+      "url": "_framework/blazor.boot.json"
+    },
+    {
+      "hash": "sha256-rcIWTVNnYus6VWf2Z2U1g/JzlC07Ym02cFjHtI59Xsc=",
+      "url": "_framework/blazor.webassembly.js"
+    },
+    {
+      "hash": "sha256-j2b3+UNNFyG53WL45TS69c81Fk1Y7Ro8OERZg9NJ+y0=",
+      "url": "_framework/dotnet.js"
+    },
+    {
+      "hash": "sha256-nElGk+CZFyq0PbPm4JU8avD6lER7x9Z1C54BK7qY12Q=",
+      "url": "_framework/dotnet.native.js"
+    },
+    {
+      "hash": "sha256-s2/s2HkJfsLLhPUvCJwiFsRUqPX4xG9g3t57WxxFR98=",
+      "url": "_framework/dotnet.native.wasm"
+    },
+    {
+      "hash": "sha256-HuKpYzsYoQBMbqMPjy15HX+ShsypvOKN9uGvoA7P/+o=",
+      "url": "_framework/dotnet.runtime.js"
+    },
+    {
+      "hash": "sha256-SZLtQnRc0JkwqHab0VUVP7T3uBPSeYzxzDnpxPpUnHk=",
+      "url": "_framework/icudt_CJK.dat"
+    },
+    {
+      "hash": "sha256-8fItetYY8kQ0ww6oxwTLiT3oXlBwHKumbeP2pRF4yTc=",
+      "url": "_framework/icudt_EFIGS.dat"
+    },
+    {
+      "hash": "sha256-L7sV7NEYP37/Qr2FPCePo5cJqRgTXRwGHuwF5Q+0Nfs=",
+      "url": "_framework/icudt_no_CJK.dat"
+    },
+    {
+      "hash": "sha256-Sn+fEWn1UVWQY4iQDfTfd9IyQj7GKUR1OLl2WgJvQDA=",
+      "url": "_framework/maqsaf.Client.wasm"
+    },
+    {
+      "hash": "sha256-R826xWW7vQN6mV1n0YPyhtgRFcWbDGfYNqUuxZgVYPc=",
+      "url": "appsettings.Development.json"
+    },
+    {
+      "hash": "sha256-gX2wvy7Mp4NkxB2695Sb8lBM9HocPQ1U876BeP78Aws=",
+      "url": "appsettings.json"
+    },
+    {
+      "hash": "sha256-aaH6+RfrUfy+itHa1P61bdjSAJcn6VjF2gIAOVWG6MY=",
+      "url": "css/app.css"
+    },
+    {
+      "hash": "sha256-+RPYEl3iZOUuRNUqhC5axOKys0sw8sXS0zum5+8ZcKk=",
+      "url": "images/logo.svg"
+    },
+    {
+      "hash": "sha256-xvh8fXMVIu36q0il4/n906OTBWaBTGTPHX8UmbJ5qaY=",
+      "url": "index.html"
+    }
+  ]
+};
---
+++ service-worker.js
@@ -0,0 +1,35 @@
+/* Manifest version: c+2vnvLH */
+// Service worker publicado para maqsaf
+self.importScripts('./service-worker-assets.js');
+self.addEventListener('install',  event => event.waitUntil(onInstall(event)));
+self.addEventListener('activate', event => event.waitUntil(onActivate(event)));
+self.addEventListener('fetch',    event => event.respondWith(onFetch(event)));
+
+const cacheNamePrefix = 'offline-cache-';
+const cacheName       = `${cacheNamePrefix}${self.assetsManifest.version}`;
+const offlineAssetsInclude = [ /\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/ ];
+const offlineAssetsExclude = [ /^service-worker\.js$/ ];
+
+async function onInstall(event) {
+    self.skipWaiting();
+    const assetsRequests = self.assetsManifest.assets
+        .filter(asset => offlineAssetsInclude.some(pattern => pattern.test(asset.url)))
+        .filter(asset => !offlineAssetsExclude.some(pattern => pattern.test(asset.url)))
+        .map(asset => new Request(asset.url, { integrity: asset.hash, cache: 'no-cache' }));
+    await (await caches.open(cacheName)).addAll(assetsRequests);
+}
+
+async function onActivate(event) {
+    const cacheKeys = await caches.keys();
+    await Promise.all(cacheKeys
+        .filter(key => key.startsWith(cacheNamePrefix) && key !== cacheName)
+        .map(key => caches.delete(key)));
+}
+
+async function onFetch(event) {
+    if (event.request.method !== 'GET') return;
+    const shouldServeIndexHtml = event.request.mode === 'navigate';
+    const request = shouldServeIndexHtml ? 'index.html' : event.request;
+    const cachedResponse = await caches.match(request);
+    return cachedResponse || fetch(event.request);
+}
